3c1f6e7f9ebc786da52f73b19228a8d95447b9f6
Hans Oksendahl <hansoksendahl@gmail.com>
2023-06-08 09:00:39 -0700

    updated utils exports

--- packages/utils/src/validation/index.ts
+++ packages/utils/src/validation/index.ts
@@ -1,2 +1,6 @@
 export { default as validateHasProp } from './validate-has-prop'
+export { default as validateIsDefined } from './validate-is-defined'
+export { default as validateIsEmail } from './validate-is-email'
+export { default as validateIsEmpty } from './validate-is-empty'
+export { default as validateIsEqual } from './validate-is-equal'
 export { default as validateIsFloat } from './validate-is-float'
